25ea4c668f39a8c555dff7346e2cefc1f4019f42
Łukasz Socha <lukasz.socha@cloudsofthouse.com>
2018-03-27 17:24:25 +0200

    tutorial part 3 finished
    /public/posters/ directory with posters to the movies - added to gitignore

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Header from './Header';
+import Movie from './Movie';
+import {initialMovies} from '../movies';
+import {additionalMovies} from '../movies';
 
 class App extends Component {
+  constructor() {
+    super();
+   
+    this.state = {
+      movies: initialMovies
+    };
+
+    this.loadAdditionalMovies = this.loadAdditionalMovies.bind(this);
+  }
+
+  loadAdditionalMovies() {
+    var currentMovies = { ...this.state.movies };
+    var newMovies = Object.assign( currentMovies, additionalMovies );
+   
+    this.setState({ movies: newMovies });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +30,20 @@ class App extends Component {
         <p className="App-intro">
           Welcome to the react-movies app!
         </p>
+        <div className="movies">
+          {/*
+          <Movie title="Tomb Raider" year="2018" description="Henry Roth is a man afraid of commitment up until he meets the beautiful Lucy. They hit it off and Henry think he's finally found the girl of his dreams." poster="./posters/tomb_raider.jpg" />
+          <Movie title="Dedpool 2" year="1986" description="A high school wise guy is determined to have a day off from school, despite what the principal thinks of that." poster="./posters/deadpool2.jpg" />
+          <Movie title="Red Sparrow" year="1996" description="Story of a wonderful little girl, who happens to be a genius, and her wonderful teacher vs. the worst parents ever and the worst school principal imaginable." poster="./posters/red_sparrow.jpg" />
+          <Movie title="Outsider" year="1987" description="Spending the summer at a Catskills resort with her family, Frances 'Baby' Houseman falls in love with the camp's dance instructor, Johnny Castle." poster="./posters/outsider.jpg" />
+          */}
+          {
+            Object
+              .keys(this.state.movies)
+              .map(key => <Movie key={key} meta={this.state.movies[key]} />)
+          }
+        </div>
+        <div className="add-movies"><button onClick={this.loadAdditionalMovies}>Load more...</button></div>
       </div>
     );
   }
---
+++ src/movies.js
@@ -0,0 +1,57 @@
+// some sample movies
+const initialMovies = {
+  movie1: {
+    title: "Tomb Raider",
+    year: "2018",
+    description: "A high school wise guy is determined to have a day off from school, despite what the principal thinks of that.",
+    poster: "./posters/tomb_raider.jpg"
+  },
+  movie2: {
+    title: "Deadpool 2",
+    year: "2001",
+    description: "A British woman is determined to improve herself while she looks for love in a year in which she keeps a personal diary.",
+    poster: "./posters/deadpool2.jpg"
+  },
+  movie3: {
+    title: "Red Sparrow",
+    year: "2004",
+    description: "Henry Roth is a man afraid of commitment up until he meets the beautiful Lucy. They hit it off and Henry think he's finally found the girl of his dreams.",
+    poster: "./posters/red_sparrow.jpg"
+  },
+  movie4: {
+    title: "Outsider",
+    year: "1996",
+    description: "Story of a wonderful little girl, who happens to be a genius, and her wonderful teacher vs. the worst parents ever and the worst school principal imaginable.",
+    poster: "./posters/outsider.jpg"
+  }
+};
+ 
+const additionalMovies = {
+  movie5: {
+    title: "Avengers",
+    year: "1987",
+    description: "Spending the summer at a Catskills resort with her family, Frances 'Baby' Houseman falls in love with the camp's dance instructor, Johnny Castle.",
+    poster: "./posters/avengers.jpg"
+  },
+  movie6: {
+    title: "Cloverfield Lane",
+    year: "1989",
+    description: "Harry and Sally have known each other for years, and are very good friends, but they fear sex would ruin the friendship.",
+    poster: "./posters/cloverfield.jpg"
+  },
+  movie7: {
+    title: "Incredibles 2",
+    year: "2003",
+    description: "After inadvertently wreaking havoc on the elf community due to his ungainly size, a man raised as an elf at the North Pole is sent to the U.S. in search of his true identity.",
+    poster: "./posters/incredibles2.jpg"
+  },
+  movie8: {
+    title: "Ocean's 8",
+    year: "1978",
+    description: "Good girl Sandy and greaser Danny fell in love over the summer. When they unexpectedly discover they're now in the same high school, will they be able to rekindle their romance?",
+    poster: "./posters/oceans8.jpg"
+  }
+};
+ 
+export {initialMovies};
+export {additionalMovies};
